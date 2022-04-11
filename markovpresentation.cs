18b31f8209e3775e869dac286523bf25ade3e2cb
Awnder <50969052+Awnder@users.noreply.github.com>
2022-04-11 10:20:25 -0700

    Add files via upload

--- markovpresentation.cs
+++ markovpresentation.cs
@@ -16,16 +16,19 @@ public class MarkovModel
         this.storyLength = storyLength;
         database = new ListSymbolTable<string, MarkovEntry>();
 
+        //loops through text to create states and assign characters to states
         for (int i = 0; i < text.Length - stateLength; i++)
         {
             string state = text.Substring(i, stateLength);
             char next = text[i+stateLength];
             
+            //creating new state if doesn't exist
             if (!database.Contains(state))
             {
                 database.Add(state, new MarkovEntry(state));
             }
-            
+
+            //assigning characters to state
             database[state].Add(next); 
         }
     }
