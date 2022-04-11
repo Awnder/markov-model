428e0fe0def19052208a998483604fa73a5cbbbc
Awnder <50969052+Awnder@users.noreply.github.com>
2022-04-11 09:39:31 -0700

    Add files via upload

--- markovexample.cs
+++ markovexample.cs
@@ -1,26 +1,20 @@
 using System;
 using System.IO;
-using System.Linq;
 using System.Collections.Generic;
-using MyListLibrary;
 using ListSymbolTable;
-using TreeSymbolTable;
 
 namespace Markov
 {
-    ///<summary>
-    ///Holds all of the information for Markov Model needs for tracking single n-character substring.
-    ///Contains string {state} - holds n-characters substring it represents.
-    ///Contains int {count} - total number of occurences of this substring in source text.
-    ///</summary>
     public class MarkovEntry
     {
-        private string state;
-        private int count;
-        private MyList<char> suffixes;
+        //class variables
+        private string state;          //a particular group of characters. Example with a length of 6: "in a h"   *notice it counts spaces
+        private int count;             //how many characters assigned to this state
+        private MyList<char> suffixes; //where the characters are stored
         Random rnd;
 
-        public MarkovEntry(string state)
+        //creation of a specific state
+        public MarkovEntry(string state) 
         {
             this.state = state;
             count = 0;
@@ -28,63 +22,54 @@ namespace Markov
             rnd = new Random();
         }
 
+        //adds a character to the state and updates the amount of characters the state has
         public void Add(char ch)
         {
             count++;
             suffixes.Add(ch);
         }
 
+        //gets a random letter from the state's assigned characters 
         public char RandomLetter()
         {
             return suffixes[rnd.Next(0, suffixes.Count)];
         }
-
-        public override string ToString()
-        {
-            string result = $"MarkovEntry: \'{state}\' ({count}): ";
-            int length = 5;
-            if (count > 5)
-            {
-                result += $"{suffixes[0]} {suffixes[1]} {suffixes[2]} {suffixes[3]} {suffixes[4]} ... ({count-length}) more suffixes.";
-            } else if (count == 5) {
-                result += $"{suffixes[0]} {suffixes[1]} {suffixes[2]} {suffixes[3]} {suffixes[4]}";
-            } else if (count == 0) {
-                result += $"no suffixes.";
-            } else {
-                result += $"{suffixes[0]} ... ({count-length}) more suffixes.";
-            }
-            return result;
-        }
     }
 
-    ///<summary>
-    ///Records information in a LinkedList Symbol Table.
-    ///</summary>
     public class ListMarkovModel
     {
-        string lines;
-        int markovDegreeNumber;
-        int storyLength;
-        ListSymbolTable<string, MarkovEntry> database;
+        //class variables
+        string lines;             //the lines of the text
+        int markovDegreeNumber;   //how long the state should be 
+        int storyLength;          //how long the story should be
+        ListSymbolTable<string, MarkovEntry> database;  //where the states are stored
 
-        public ListMarkovModel(string lines, int markovDegreeNumber, int storyLength)
+        public ListMarkovModel(string lines, int markovDegreeNumber, int storyLength) //user input is placed in the constructor
         {
+            //user input assigned to class variables
             this.lines = lines;
             this.markovDegreeNumber = markovDegreeNumber;
             this.storyLength = storyLength;
             database = new ListSymbolTable<string, MarkovEntry>();
 
-            //creates database
+            //creates database using for loop to read all the characters in the text. It runs until it reaches the 
+            //markovDegreeNumber + 1 of characters before the end. Example: 100 characters and the state is 5, it will end at 94
+            //this way, the program does not go out of bounds by reading over the 100 characters
             for (int i = 0; i < lines.Length - markovDegreeNumber; i++) //beginning; end - length of state; increment;
             {
-                string state = lines.Substring(i, markovDegreeNumber);
-                char next = lines[i+markovDegreeNumber];
-                //construct MarkovModel
-                if (!database.Contains(state))
+                //the program will read until 94. the state will look at 95-99, the character will look at 100
+                string state = lines.Substring(i, markovDegreeNumber); //if the state is 5, then it will record 5 characters of the text
+                char next = lines[i+markovDegreeNumber]; //this is why there is a +1. it reads one character in front of the state
+                
+                if (!database.Contains(state)) //if the database does not contain the state
                 {
-                    database.Add(state, new MarkovEntry(state));
+                    database.Add(state, new MarkovEntry(state)); //then add it
                 }
-                database[state].Add(next);
+
+                //assign the characters that come after the state, to the state. if the state is "in a h" the 
+                //next characters could be 'a' or 'o' for "in a hat" or "in a house"
+                //when the program sees this, it will assign the 'a' and the 'o' to the state
+                database[state].Add(next); 
             }
         }
 
