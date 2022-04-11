bdc56ff4458b02026d2c61718ef22f398bea236b
Awnder <50969052+Awnder@users.noreply.github.com>
2022-04-11 10:18:31 -0700

    Add files via upload

---
+++ markovpresentation.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using ListSymbolTable;
+
+public class MarkovModel
+{
+    string text;
+    int stateLength;
+    int storyLength;
+    ListSymbolTable<string, MarkovEntry> database;
+
+    public MarkovModel(string text, int stateLength, int storyLength)
+    {
+        this.text = text;
+        this.stateLength = stateLength;
+        this.storyLength = storyLength;
+        database = new ListSymbolTable<string, MarkovEntry>();
+
+        for (int i = 0; i < text.Length - stateLength; i++)
+        {
+            string state = text.Substring(i, stateLength);
+            char next = text[i+stateLength];
+            
+            if (!database.Contains(state))
+            {
+                database.Add(state, new MarkovEntry(state));
+            }
+            
+            database[state].Add(next); 
+        }
+    }
+}
