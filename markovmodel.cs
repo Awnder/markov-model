cfb0586558830925855045d4e64208ff9e8d1f56
Awnder <50969052+Awnder@users.noreply.github.com>
2022-04-12 12:55:18 -0700

    Add files via upload

--- markovmodel.cs
+++ markovmodel.cs
@@ -10,8 +10,6 @@ namespace Markov
 {
     ///<summary>
     ///Holds all of the information for Markov Model needs for tracking single n-character substring.
-    ///Contains string {state} - holds n-characters substring it represents.
-    ///Contains int {count} - total number of occurences of this substring in source text.
     ///</summary>
     public class MarkovEntry
     {
@@ -20,6 +18,9 @@ namespace Markov
         private MyList<char> suffixes;
         Random rnd;
 
+        ///<summary>
+        ///Constructor takes string state and initializes private variables.
+        ///</summary>
         public MarkovEntry(string state)
         {
             this.state = state;
@@ -28,17 +29,27 @@ namespace Markov
             rnd = new Random();
         }
 
+        ///<summary>
+        ///Increases the amount of characters and adds the character to the LinkedList.
+        ///</summary>
         public void Add(char ch)
         {
             count++;
             suffixes.Add(ch);
         }
 
+        ///<summary>
+        ///Gets a random number between 0 and the list's length, then uses that number to search through the list and 
+        ///returns the character at the specified index.
+        ///</summary>
         public char RandomLetter()
         {
             return suffixes[rnd.Next(0, suffixes.Count)];
         }
 
+        ///<summary>
+        ///Prints out the state, the amount of characters associated with that state, and some of the characters.
+        ///</summary>
         public override string ToString()
         {
             string result = $"MarkovEntry: \'{state}\' ({count}): ";
@@ -57,17 +68,24 @@ namespace Markov
         }
     }
 
+    ///<summary>
+    ///Abstract class inherited by the other three models.
+    ///Contains variables consistent that the other three need so less copy/pasting code
+    ///</summary>
     public abstract class MarkovModel
     {
         protected string lines;
         protected int markovDegreeNumber;
         protected int storyLength;
-        protected int totalChar = 0; // tracks total characters when flow over storyLength
+        protected int totalChar = 0; //tracks total characters when flow over storyLength
         protected string endings = ".?!";
         protected string capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         protected Random rnd = new Random();
         protected MyList<string> wordDictionary = new MyList<string>();
 
+        ///<summary>
+        ///Constructor takes the lines of text from file, the length of the state, and the length of the story
+        ///</summary>
         public MarkovModel(string lines, int markovDegreeNumber, int storyLength)
         {
             this.lines = lines;
@@ -91,6 +109,10 @@ namespace Markov
     {
         ListSymbolTable<string, MarkovEntry> database;
 
+        ///<summary>
+        ///Constructor implements abstract class. see ": base (lines, markovDegreeNumber, storyLength)"
+        ///Reads through file and stores every state and character associated with that state in a SymbolTable database
+        ///</summary>
         public ListMarkovModel(string lines, int markovDegreeNumber, int storyLength) : base (lines, markovDegreeNumber, storyLength)
         {
             database = new ListSymbolTable<string, MarkovEntry>();
@@ -111,28 +133,33 @@ namespace Markov
             // CreateWordDictionary();
         }
 
+        ///<summary>
+        ///Records information in a LinkedList Symbol Table.
+        ///</summary>
         public override string CreateStory()
         {
-            string story = "";
-            string state = "";
-            char c;
-            int count = 0;
+            string story = ""; //stores the story as a whole
+            string state = ""; //stores each state that program iterates through
+            char c;            //stores each character generated according to each state
+            int count = 0;     //stores amount of characters generated
 
             //first state
-            // state = lines.Substring(0, markovDegreeNumber);
+            //state = lines.Substring(0, markovDegreeNumber);
             state = RandomStart();
             c = database[state].RandomLetter();
-            story = story + state + c;
+            story = story + state + c; //adds the beginning to the start of the story as well as the next character
             state = IncrementState(state, c);
             totalChar = totalChar + markovDegreeNumber + 1;
 
+            //loops until the next character is ".?!" or the count < storyLength
             while (!endings.Contains(c) || count < storyLength)
             {
+                //last check to see if the state is the right length and the state is actually in the database
                 if (state.Length == markovDegreeNumber && database[state] != null)
                 {
-                    c = database[state].RandomLetter();
-                    story = story + c;
-                    state = IncrementState(state, c);
+                    c = database[state].RandomLetter(); //gets a random letter
+                    story = story + c; //adds the letter to the story
+                    state = IncrementState(state, c); //moves the state to the next one forward
                     count++;
                     totalChar++;
                 }
@@ -140,6 +167,9 @@ namespace Markov
             return story;
         }
 
+        ///<summary>
+        ///Finds a random state that starts with a capital letter and does not have a space in it.
+        ///</summary>
         public override string RandomStart()
         {
             int start = rnd.Next(0, lines.Length - markovDegreeNumber);
@@ -152,6 +182,9 @@ namespace Markov
             return state;
         }
 
+        ///<summary>
+        ///Adds the next character to the state, then cuts of the first character of the state to retain correct state length.
+        ///</summary>
         public override string IncrementState(string state, char c)
         {
             state += c;
@@ -181,6 +214,9 @@ namespace Markov
         //     }
         // }
 
+        ///<summary>
+        ///Returns amount of database nodes.
+        ///</summary>
         public override string ToString()
         {
             return database.Count + " nodes";
