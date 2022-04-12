bdeac5c6ca0f87afd86b5bc7fc2bd3ce7061e9f0
Awnder <50969052+Awnder@users.noreply.github.com>
2022-04-12 14:36:57 -0800

    Add files via upload

--- markovmodel.cs
+++ markovmodel.cs
@@ -15,18 +15,16 @@ namespace Markov
     {
         private string state;
         private int count;
-        private MyList<char> suffixes;
-        Random rnd;
+        private MyList<char> suffixes = new MyList<char>();
+        Random rnd = new Random();
 
         ///<summary>
-        ///Constructor takes string state and initializes private variables.
+        ///Constructor takes string state and initializes count to 0.
         ///</summary>
         public MarkovEntry(string state)
         {
             this.state = state;
             count = 0;
-            suffixes = new MyList<char>();
-            rnd = new Random();
         }
 
         ///<summary>
@@ -69,74 +67,46 @@ namespace Markov
     }
 
     ///<summary>
-    ///Abstract class inherited by the other three models.
-    ///Contains variables consistent that the other three need so less copy/pasting code
-    ///</summary>
-    public abstract class MarkovModel
-    {
-        protected string lines;
-        protected int markovDegreeNumber;
-        protected int storyLength;
-        protected int totalChar = 0; //tracks total characters when flow over storyLength
-        protected string endings = ".?!";
-        protected string capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        protected Random rnd = new Random();
-        protected MyList<string> wordDictionary = new MyList<string>();
-
-        ///<summary>
-        ///Constructor takes the lines of text from file, the length of the state, and the length of the story
-        ///</summary>
-        public MarkovModel(string lines, int markovDegreeNumber, int storyLength)
-        {
-            this.lines = lines;
-            this.markovDegreeNumber = markovDegreeNumber;
-            this.storyLength = storyLength;
-        }
-
-        public abstract string CreateStory();
-        public abstract string IncrementState(string state, char c);
-        public abstract string RandomStart();
-        // public abstract void CreateWordDictionary();
-        // public abstract bool ValidWord(string state);
-
-        public int TotalChar { get {return totalChar;} }
-    }
-
-    ///<summary>
     ///Records information in a LinkedList Symbol Table.
     ///</summary>
-    public class ListMarkovModel : MarkovModel
+    public class ListMarkovModel
     {
-        ListSymbolTable<string, MarkovEntry> database;
+        string lines;       //lines from the text
+        int stateLength;    //state length
+        int storyLength;    //story length
+        int totalChar = 0;  //tracks total characters when flow over storyLength
+        string endings = ".?!";
+        string capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Random rnd = new Random();
+        ListSymbolTable<string, MarkovEntry> database = new ListSymbolTable<string, MarkovEntry>();
 
         ///<summary>
-        ///Constructor implements abstract class. see ": base (lines, markovDegreeNumber, storyLength)"
         ///Reads through file and stores every state and character associated with that state in a SymbolTable database
         ///</summary>
-        public ListMarkovModel(string lines, int markovDegreeNumber, int storyLength) : base (lines, markovDegreeNumber, storyLength)
+        public ListMarkovModel(string lines, int stateLength, int storyLength)
         {
-            database = new ListSymbolTable<string, MarkovEntry>();
+            this.lines = lines;
+            this.stateLength = stateLength;
+            this.storyLength = storyLength;
 
             //creates database
-            for (int i = 0; i < lines.Length - markovDegreeNumber; i++) //beginning; end - length of state; increment;
+            for (int i = 0; i < lines.Length - stateLength; i++) //beginning; end - length of state; increment;
             {
-                string state = lines.Substring(i, markovDegreeNumber);
-                char next = lines[i+markovDegreeNumber];
+                string state = lines.Substring(i, stateLength);
+                char next = lines[i+stateLength];
                 //construct MarkovModel
-                if (!database.Contains(state))
+                if (!database.Contains(state)) //if database doesn't have state
                 {
-                    database.Add(state, new MarkovEntry(state));
+                    database.Add(state, new MarkovEntry(state)); //create new one
                 }
-                database[state].Add(next);
+                database[state].Add(next); //add characters to the state in the database
             }
-
-            // CreateWordDictionary();
         }
 
         ///<summary>
         ///Records information in a LinkedList Symbol Table.
         ///</summary>
-        public override string CreateStory()
+        public string CreateStory()
         {
             string story = ""; //stores the story as a whole
             string state = ""; //stores each state that program iterates through
@@ -144,18 +114,17 @@ namespace Markov
             int count = 0;     //stores amount of characters generated
 
             //first state
-            //state = lines.Substring(0, markovDegreeNumber);
-            state = RandomStart();
+            state = lines.Substring(0, stateLength);
             c = database[state].RandomLetter();
             story = story + state + c; //adds the beginning to the start of the story as well as the next character
             state = IncrementState(state, c);
-            totalChar = totalChar + markovDegreeNumber + 1;
+            totalChar = totalChar + stateLength + 1;
 
             //loops until the next character is ".?!" or the count < storyLength
-            while (!endings.Contains(c) || count < storyLength)
+            while (count < storyLength)
             {
                 //last check to see if the state is the right length and the state is actually in the database
-                if (state.Length == markovDegreeNumber && database[state] != null)
+                if (state.Length == stateLength && database[state] != null)
                 {
                     c = database[state].RandomLetter(); //gets a random letter
                     story = story + c; //adds the letter to the story
@@ -168,51 +137,14 @@ namespace Markov
         }
 
         ///<summary>
-        ///Finds a random state that starts with a capital letter and does not have a space in it.
-        ///</summary>
-        public override string RandomStart()
-        {
-            int start = rnd.Next(0, lines.Length - markovDegreeNumber);
-            string state = lines.Substring(start, markovDegreeNumber);
-            while (state.Contains(' ') || !capitals.Contains(state.Substring(0,1)))
-            {
-                start = rnd.Next(0, lines.Length - markovDegreeNumber);
-                state = lines.Substring(start, markovDegreeNumber);
-            }
-            return state;
-        }
-
-        ///<summary>
         ///Adds the next character to the state, then cuts of the first character of the state to retain correct state length.
         ///</summary>
-        public override string IncrementState(string state, char c)
+        public string IncrementState(string state, char c)
         {
             state += c;
-            state = state.Substring(1); //gets all char after the first to retain correct MarkovDegreeNumber length
+            state = state.Substring(1);
             return state;
         }
-
-        // public override void CreateWordDictionary()
-        // {
-        //     foreach(string state in database)
-        //     {
-        //         string trimmed = String.Concat(state.Where(c => !Char.IsWhiteSpace(c)));
-        //         if (trimmed.Length == markovDegreeNumber)
-        //         {
-        //             wordDictionary.Add(trimmed);
-        //         }
-        //     }
-        // } 
-
-        // public override bool ValidWord(string state)
-        // {
-        //     if (wordDictionary.Contains(state))
-        //     {
-        //         return true;
-        //     } else {
-        //         return false;
-        //     }
-        // }
 
         ///<summary>
         ///Returns amount of database nodes.
@@ -230,15 +162,15 @@ namespace Markov
     {
         TreeSymbolTable<string, MarkovEntry> database;
 
-        public TreeMarkovModel(string lines, int markovDegreeNumber, int storyLength) : base (lines, markovDegreeNumber, storyLength)
+        public TreeMarkovModel(string lines, int stateLength, int storyLength) : base (lines, stateLength, storyLength)
         {
             database = new TreeSymbolTable<string, MarkovEntry>();
 
             //creates database
-            for (int i = 0; i < lines.Length - markovDegreeNumber; i++) //beginning; end - length of state; increment;
+            for (int i = 0; i < lines.Length - stateLength; i++) //beginning; end - length of state; increment;
             {
-                string state = lines.Substring(i, markovDegreeNumber);
-                char next = lines[i+markovDegreeNumber];
+                string state = lines.Substring(i, stateLength);
+                char next = lines[i+stateLength];
                 //construct MarkovModel
                 if (!database.ContainsKey(state))
                 {
@@ -248,7 +180,7 @@ namespace Markov
             }
         }
 
-        public override string CreateStory()
+        public string CreateStory()
         {
             string story = "";
             string state = "";
@@ -260,11 +192,11 @@ namespace Markov
             c = database[state].RandomLetter();
             story = story + state + c;
             state = IncrementState(state, c);
-            totalChar = totalChar + markovDegreeNumber + 1;
+            totalChar = totalChar + stateLength + 1;
 
             while (!endings.Contains(c) || count < storyLength)
             {
-                if (state.Length == markovDegreeNumber && database[state] != null) //error checking just in case states not stored or retrieved correctly
+                if (state.Length == stateLength && database[state] != null) //error checking just in case states not stored or retrieved correctly
                 {
                     c = database[state].RandomLetter();
                     story = story + c;
@@ -276,23 +208,23 @@ namespace Markov
             return story;
         }
 
-        public override string RandomStart()
+        public string RandomStart()
         {
-            int start = rnd.Next(0, lines.Length - markovDegreeNumber);
-            string state = lines.Substring(start, markovDegreeNumber);
+            int start = rnd.Next(0, lines.Length - stateLength);
+            string state = lines.Substring(start, stateLength);
             // finds state without spaces in it and with a beginning capital letter
             while (state.Contains(' ') || !capitals.Contains(state.Substring(0,1)))
             {
-                start = rnd.Next(0, lines.Length - markovDegreeNumber);
-                state = lines.Substring(start, markovDegreeNumber);
+                start = rnd.Next(0, lines.Length - stateLength);
+                state = lines.Substring(start, stateLength);
             }
             return state;
         }
 
-        public override string IncrementState(string state, char c)
+        public string IncrementState(string state, char c)
         {
             state += c;
-            state = state.Substring(1); //gets all char after the first to retain correct MarkovDegreeNumber length
+            state = state.Substring(1); //gets all char after the first to retain correct stateLength length
             return state;
         }
 
@@ -309,15 +241,15 @@ namespace Markov
     {
         SortedDictionary<string, MarkovEntry> database;
 
-        public DictMarkovModel(string lines, int markovDegreeNumber, int storyLength) : base (lines, markovDegreeNumber, storyLength)
+        public DictMarkovModel(string lines, int stateLength, int storyLength) : base (lines, stateLength, storyLength)
         {
             database = new SortedDictionary<string, MarkovEntry>();
 
             //creates database
-            for (int i = 0; i < lines.Length - markovDegreeNumber; i++) //beginning; end - length of state; increment;
+            for (int i = 0; i < lines.Length - stateLength; i++) //beginning; end - length of state; increment;
             {
-                string state = lines.Substring(i, markovDegreeNumber);
-                char next = lines[i+markovDegreeNumber];
+                string state = lines.Substring(i, stateLength);
+                char next = lines[i+stateLength];
                 //construct MarkovModel
                 if (!database.ContainsKey(state))
                 {
@@ -327,7 +259,7 @@ namespace Markov
             }
         }
 
-        public override string CreateStory()
+        public string CreateStory()
         {
             string story = "";
             string state = "";
@@ -339,11 +271,11 @@ namespace Markov
             c = database[state].RandomLetter();
             story = story + state + c;
             state = IncrementState(state, c);
-            totalChar = totalChar + markovDegreeNumber + 1;
+            totalChar = totalChar + stateLength + 1;
 
             while (!endings.Contains(c) || count < storyLength)
             {
-                if (state.Length == markovDegreeNumber && database[state] != null)
+                if (state.Length == stateLength && database[state] != null)
                 {
                     c = database[state].RandomLetter();
                     story = story + c;
@@ -355,22 +287,22 @@ namespace Markov
             return story;
         }
 
-        public override string RandomStart()
+        public string RandomStart()
         {
-            int start = rnd.Next(0, lines.Length - markovDegreeNumber);
-            string state = lines.Substring(start, markovDegreeNumber);
+            int start = rnd.Next(0, lines.Length - stateLength);
+            string state = lines.Substring(start, stateLength);
             while (state.Contains(' ') || !capitals.Contains(state.Substring(0,1)))
             {
-                start = rnd.Next(0, lines.Length - markovDegreeNumber);
-                state = lines.Substring(start, markovDegreeNumber);
+                start = rnd.Next(0, lines.Length - stateLength);
+                state = lines.Substring(start, stateLength);
             }
             return state;
         }
 
-        public override string IncrementState(string state, char c)
+        public string IncrementState(string state, char c)
         {
             state += c;
-            state = state.Substring(1); //gets all char after the first to retain correct MarkovDegreeNumber length
+            state = state.Substring(1); //gets all char after the first to retain correct stateLength length
             return state;
         }
 
